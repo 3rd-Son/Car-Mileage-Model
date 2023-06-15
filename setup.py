b8f0ef82cb26b01df0ff61080b36910f474f3ff3
Victory Nnaji <nnajivictorious@gmail.com>
2023-06-15 11:49:04 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A model to predict the mileage of cas using the available features of the car',
+    author='vic3sax',
+    license='MIT',
+)
